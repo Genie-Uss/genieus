f93c3a140c775e738555625eeabf302612af591f
KIM SOOBIN <xpsxm225@naver.com>
2025-04-08 19:58:23 +0900

    Feat: User 서비스와 공유하는 암호화 및 vo를 정의
    - User의 Email, Role의 필드 값을 그대로 사용하므로, vo를 추상화시켜 공통 라이브러리를 활용할 계획입니다.
    - 암호화의 경우 두 서비스가 공유해야할 부분이므로, 공통 라이브러리로 이동시킬 계획입니다.
    - RoleType은 genieus 공통 라이브러리에 맞게 수정하여 업로드할 계획입니다.
    
    Resolved #9

---
+++ auth-service/src/main/java/shop/genieus/auth/global/common/GenieusPasswordEncoder.java
@@ -0,0 +1,17 @@
+package shop.genieus.auth.global.common;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenieusPasswordEncoder {
+  private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+  public String encode(CharSequence rawPassword) {
+    return encoder.encode(rawPassword);
+  }
+
+  public boolean matches(CharSequence rawPassword, String encodedPassword) {
+    return encoder.matches(rawPassword, encodedPassword);
+  }
+}
