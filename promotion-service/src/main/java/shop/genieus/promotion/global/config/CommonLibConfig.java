628c29736d95c54c8164c631b53c5916356f0710
sosa7753 <sosa7753@naver.com>
2025-04-10 22:45:03 +0900

    feat: 공통 라이브러리 Config 추가

---
+++ promotion-service/src/main/java/shop/genieus/promotion/global/config/CommonLibConfig.java
@@ -0,0 +1,9 @@
+package shop.genieus.promotion.global.config;
+
+import com.genieus.common.annotation.EnableCommonLib;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCommonLib
+public class CommonLibConfig {
+}
