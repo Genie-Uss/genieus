61b325b883da92f5d896651b18a28197990b0156
zun <k5orkrj@gmail.com>
2025-04-10 21:57:41 +0900

    Chore: Common 라이브러리 셋업

---
+++ payment-service/src/main/java/shop/genieus/payment/global/config/CommonLibConfig.java
@@ -0,0 +1,8 @@
+package shop.genieus.payment.global.config;
+
+import com.genieus.common.annotation.EnableCommonLib;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCommonLib
+public class CommonLibConfig {}
