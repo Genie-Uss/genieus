eac413a7b7a21e22c40127a9f0ef9db1c0291414
sosa7753 <sosa7753@naver.com>
2025-04-10 22:49:05 +0900

    feat: GlobalExceptionHandler 추가

--- promotion-service/src/main/java/shop/genieus/promotion/global/handler/GlobalExceptionHandler.java
+++ promotion-service/src/main/java/shop/genieus/promotion/global/handler/GlobalExceptionHandler.java
@@ -2,12 +2,13 @@ package shop.genieus.promotion.global.handler;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+import com.genieus.common.response.ApiResponse;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import shop.genieus.promotion.global.common.ApiResponse;
 import shop.genieus.promotion.global.exception.PromotionException;
 
 @RestControllerAdvice
@@ -33,5 +34,17 @@ public class GlobalExceptionHandler {
 
     String errorMessages = sb.toString();
     return ResponseEntity.status(BAD_REQUEST).body(ApiResponse.fail(3000, errorMessages));
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("런타임 오류 발생: " + e.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("예상치 못한 오류 발생: " + e.getMessage());
   }
 }
