79bcb6264b1f50f4436a7c2fd328f928372be6ea
Soobin Kim <xpsxm225@naver.com>
2025-04-10 15:16:19 +0900

    Feat: 인증 서비스 클라이언트 및 WebClient 설정 추가
    - 인증 서비스 통신을 위한 클라이언트 코드 구현
    - 토큰 기반 인가 처리를 위한 인증 서비스 클라이언트 추가
    - 분산 추적을 위한 Tracer 연동 WebClient 설정 구현
    - 서비스 간 통신을 위한 로드밸런서 지원 WebClient 빈 등록
    
    Resolved #21

---
+++ api-gateway/src/main/java/shop/genieus/gateway/client/AuthServiceClient.java
@@ -0,0 +1,47 @@
+package shop.genieus.gateway.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+import shop.genieus.gateway.client.dto.AuthClientRequest;
+import shop.genieus.gateway.client.dto.AuthClientResponse;
+import shop.genieus.gateway.constants.AppConstants.ApiPath.Internal;
+
+@Slf4j
+@Component
+public class AuthServiceClient {
+  private final String authServiceUrl = "http://AUTH-SERVICE";
+  private final WebClient.Builder webClientBuilder;
+
+  public AuthServiceClient(WebClient.Builder webClientBuilder) {
+    this.webClientBuilder = webClientBuilder;
+  }
+
+  public Mono<AuthClientResponse> authorize(String token, String uri, String method) {
+    log.debug("인가 요청: uri={}, method={}", uri, method);
+
+    return webClientBuilder
+        .build()
+        .post()
+        .uri(authServiceUrl + Internal.AUTH_ISSUE_PASSPORT)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new AuthClientRequest(token, uri, method))
+        .retrieve()
+        .bodyToMono(AuthClientResponse.class)
+        .doOnNext(response -> log.debug("인가 응답: {}", response.toString()))
+        .doOnError(
+            e -> {
+              log.error("인가 처리 오류: {}", e.getMessage());
+              if (e instanceof WebClientResponseException) {
+                WebClientResponseException wcre = (WebClientResponseException) e;
+                log.error(
+                    "응답 상태 코드: {}, 응답 본문: {}",
+                    wcre.getStatusCode(),
+                    wcre.getResponseBodyAsString());
+              }
+            });
+  }
+}
