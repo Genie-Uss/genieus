eb163a60fa180cd242c371139a92dd22f8e7ed6e
sosa7753 <sosa7753@naver.com>
2025-04-11 14:43:02 +0900

    feat: 공통 라이브러리, Auditing Config 추가

---
+++ product-service/src/main/java/shop/genieus/product/global/config/CommonLibConfig.java
@@ -0,0 +1,9 @@
+package shop.genieus.product.global.config;
+
+import com.genieus.common.annotation.EnableCommonLib;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCommonLib
+public class CommonLibConfig {
+}
