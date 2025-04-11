4f66752c5993f5e8e67d90f4df6a4584bd40f04b
sosa7753 <sosa7753@naver.com>
2025-04-11 14:43:31 +0900

    feat: Product 예외 및 글로벌 예외추가

---
+++ product-service/src/main/java/shop/genieus/product/global/constants/Code.java
@@ -0,0 +1,15 @@
+package shop.genieus.product.global.constants;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Code {
+
+  CREATE_PRODUCT_SUCCESS(2101, "상품이 생성되었습니다."),
+  ;
+
+  private final Integer code;
+  private final String message;
+}
