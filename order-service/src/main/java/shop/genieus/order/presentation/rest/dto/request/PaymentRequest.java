77a7495b56435403e7672ead1af3f4bf2e07fc0f
seongmin <seongmin1117@gmail.com>
2025-04-11 22:14:40 +0900

    Fix: 결제요청시 결제수단 삭제

--- order-service/src/main/java/shop/genieus/order/presentation/rest/dto/request/PaymentRequest.java
+++ order-service/src/main/java/shop/genieus/order/presentation/rest/dto/request/PaymentRequest.java
@@ -1,18 +1,14 @@
 package shop.genieus.order.presentation.rest.dto.request;
 
 import com.genieus.common.auth.model.Passport;
-import jakarta.validation.constraints.NotBlank;
 import shop.genieus.order.application.in.command.dto.PaymentCommand;
 
-public record PaymentRequest(
-    Long couponId, @NotBlank(message = "결제수단은 필수입니다.") String paymentMethod) {
+public record PaymentRequest(Long couponId) {
   public PaymentCommand toCommand(Passport passport, Long orderId) {
     return PaymentCommand.builder()
         .userId(passport.getUserId())
-        .role(passport.getRole())
         .orderId(orderId)
         .couponId(couponId)
-        .paymentMethod(paymentMethod)
         .build();
   }
 }
