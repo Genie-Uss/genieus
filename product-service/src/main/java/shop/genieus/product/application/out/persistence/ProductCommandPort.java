7bb3f1e57097d209aa42f9fb710b4ce4468cdf75
Soobin Kim <xpsxm225@naver.com>
2025-04-13 16:37:16 +0900

    Update: import 구문 추가

--- product-service/src/main/java/shop/genieus/product/application/out/persistence/ProductCommandPort.java
+++ product-service/src/main/java/shop/genieus/product/application/out/persistence/ProductCommandPort.java
@@ -1,12 +1,11 @@
 package shop.genieus.product.application.out.persistence;
 
 import java.util.List;
-import shop.genieus.product.application.in.command.dto.CreateProductCommand;
 import shop.genieus.product.domain.model.entity.Product;
 
 public interface ProductCommandPort {
 
   Product save(Product product);
 
-  List<Product> findProductsByIds(java.util.List<java.lang.Long> longs);
+  List<Product> findProductsByIds(List<Long> longs);
 }
