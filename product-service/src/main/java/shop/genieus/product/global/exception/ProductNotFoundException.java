424126ae61f206076785399c42d914dcdb699029
Soobin Kim <xpsxm225@naver.com>
2025-04-14 22:06:53 +0900

    Update: 상품 관련 예외에서 상세 내용을 제외시킴

--- product-service/src/main/java/shop/genieus/product/global/exception/ProductNotFoundException.java
+++ product-service/src/main/java/shop/genieus/product/global/exception/ProductNotFoundException.java
@@ -7,19 +7,7 @@ public class ProductNotFoundException extends ProductException {
   private static final String PRODUCT_NOT_FOUND = "상품을 찾을 수 없거나 판매 상태 중이 아닙니다.";
   private static final int PRODUCT_NOT_FOUND_CODE = 2001;
 
-  private ProductNotFoundException(String additionalMessage) {
-    super(PRODUCT_NOT_FOUND + ", " + additionalMessage, PRODUCT_NOT_FOUND_CODE);
-  }
-
-  private ProductNotFoundException() {
+  public ProductNotFoundException() {
     super(PRODUCT_NOT_FOUND, PRODUCT_NOT_FOUND_CODE);
-  }
-
-  public static ProductNotFoundException create() {
-    return new ProductNotFoundException();
-  }
-
-  public static ProductNotFoundException create(String additionalMessage) {
-    return new ProductNotFoundException(additionalMessage);
   }
 }
