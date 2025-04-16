57a139697c602025b6303cf713c857c670f26fbe
Soobin Kim <xpsxm225@naver.com>
2025-04-16 14:05:36 +0900

    Feat: auth, user feign 통신 관련한 에러를 글로벌 오류 처리기에 등록

--- user-service/src/main/java/shop/genieus/user/global/handler/GlobalExceptionHandler.java
+++ user-service/src/main/java/shop/genieus/user/global/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package shop.genieus.user.global.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.genieus.common.auth.exception.UnauthorizedException;
 import com.genieus.common.response.ApiResponse;
+import feign.FeignException.FeignClientException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,16 +25,17 @@ import shop.genieus.user.global.exception.UserException;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
   private static final int SERVER_ERROR_CODE = 1999;
   private static final int VALIDATION_ERROR_CODE = 1998;
   private static final int REQUEST_PARSING_ERROR_CODE = 1997;
   private static final int UNAUTHORIZED_ERROR_CODE = 7999;
-
   private static final String SERVER_ERROR_MESSAGE = "서버 내부 오류가 발생했습니다";
   private static final String VALIDATION_ERROR_MESSAGE = "입력값이 유효하지 않습니다";
   private static final String REQUEST_PARSING_ERROR_MESSAGE = "요청 본문을 읽을 수 없습니다";
+
+  private final ObjectMapper objectMapper;
 
   @ExceptionHandler(UserException.class)
   public ResponseEntity<ApiResponse<Void>> handleUserException(
@@ -53,6 +57,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         UNAUTHORIZED_ERROR_CODE);
 
     return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+  }
+
+  @ExceptionHandler(FeignClientException.class)
+  protected ResponseEntity<ApiResponse<Void>> handleFeignClientException(FeignClientException e) {
+    try {
+      String content = e.contentUTF8();
+      ApiResponse<?> response = objectMapper.readValue(content, ApiResponse.class);
+      log.error("{} 예외 발생: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+      return ResponseEntity.status(e.status())
+          .body(ApiResponse.fail(response.code(), response.message()));
+    } catch (Exception ex) {
+      log.error("{} 예외 발생: {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
+      return ResponseEntity.status(e.status())
+          .body(ApiResponse.fail(e.status(), "Feign 예외 발생 (Body 파싱 실패)"));
+    }
   }
 
   @ExceptionHandler(RuntimeException.class)
