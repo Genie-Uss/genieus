9ca13cc0e877b75f94f7f5191c4cf8b296dc56f6
ryujy <bamdal124@gmail.com>
2025-04-18 00:08:14 +0900

    Feat: Coupon - batch job 설정 파일 작성
    - 배치 작업에 필요한 job과 step 설정
    - 배치 작업에 필요한 reader, writer 파일 작성
    - Job -> Step -> Reader, Processor, Writer 구조 동작

---
+++ coupon-service/src/main/java/shop/genieus/coupon/global/config/BatchJobConfig.java
@@ -0,0 +1,37 @@
+package shop.genieus.coupon.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import shop.genieus.coupon.infrastructure.persistence.dto.IssueCouponCommand;
+import shop.genieus.coupon.infrastructure.persistence.repository.CouponRedisReader;
+import shop.genieus.coupon.infrastructure.persistence.repository.CouponRedisWrite;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchJobConfig {
+  private final CouponRedisWrite writer;
+  private final CouponRedisReader reader;
+
+  @Bean
+  public Job couponSaveJob(JobRepository jobRepository, Step saveCouponStep) {
+    return new JobBuilder("couponSaveJob", jobRepository).start(saveCouponStep).build();
+  }
+
+  @Bean
+  public Step saveCouponStep(
+      JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    // reader를 100번 호출해서 100개의 데이터를 모은 다음 writer를 호출
+    return new StepBuilder("saveCouponStep", jobRepository)
+        .<IssueCouponCommand, IssueCouponCommand>chunk(100, transactionManager)
+        .reader(reader)
+        .writer(writer)
+        .build();
+  }
+}
