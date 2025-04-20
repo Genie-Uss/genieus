60136603fbfa2913cdc79135c86765ed7596d184
Soobin Kim <xpsxm225@naver.com>
2025-04-20 15:44:46 +0900

    Update: 상품 재고 조회 서비스의 반환 값 변경에 따른 코드 리팩토링
    Resolved #61

--- product-service/src/main/java/shop/genieus/product/presentation/rest/mapper/ProductInternalMapper.java
+++ product-service/src/main/java/shop/genieus/product/presentation/rest/mapper/ProductInternalMapper.java
@@ -2,11 +2,13 @@ package shop.genieus.product.presentation.rest.mapper;
 
 import com.genieus.common.internal.request.StockRequest;
 import com.genieus.common.internal.response.ProductClientResponse;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
-import shop.genieus.product.application.in.command.dto.StockValidationResult;
 import shop.genieus.product.application.in.command.dto.ValidateProductCommand;
+import shop.genieus.product.domain.model.ProductView;
 import shop.genieus.product.domain.model.entity.Product;
 
 @Component
@@ -19,24 +21,33 @@ public class ProductInternalMapper {
         product.getProductPrice().getValue());
   }
 
-  public ProductClientResponse toStockValidationResult(
-      StockValidationResult stockValidationResult) {
+  public ProductClientResponse toProductClientResponse(ProductView product) {
     return new ProductClientResponse(
-        stockValidationResult.productId(),
-        stockValidationResult.name(),
-        stockValidationResult.text(),
-        stockValidationResult.price());
+        product.getProductId(),
+        product.getProductName(),
+        product.getProductText(),
+        product.getProductPrice());
+  }
+
+  public List<ProductClientResponse> toClientResponseListFromProductViewList(
+      List<ProductView> productViewList) {
+    if (productViewList == null || productViewList.isEmpty()) {
+      return Collections.emptyList();
+    }
+    return productViewList.stream()
+        .filter(Objects::nonNull)
+        .map(this::toProductClientResponse)
+        .collect(Collectors.toList());
   }
 
   public List<ProductClientResponse> toClientResponseListFromProductList(
       List<Product> productList) {
-    return productList.stream().map(this::toProductClientResponse).collect(Collectors.toList());
-  }
-
-  public List<ProductClientResponse> toClientResponseListFromValidationResultList(
-      List<StockValidationResult> stockValidationResults) {
-    return stockValidationResults.stream()
-        .map(this::toStockValidationResult)
+    if (productList == null || productList.isEmpty()) {
+      return Collections.emptyList();
+    }
+    return productList.stream()
+        .filter(Objects::nonNull)
+        .map(this::toProductClientResponse)
         .collect(Collectors.toList());
   }
 
