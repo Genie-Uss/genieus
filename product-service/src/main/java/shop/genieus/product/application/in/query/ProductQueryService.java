979caa60d45595f1946556c7fccdc39fb747b5e1
Soobin Kim <xpsxm225@naver.com>
2025-04-20 15:39:02 +0900

    Feat: 상품 query service를 추가

---
+++ product-service/src/main/java/shop/genieus/product/application/in/query/ProductQueryService.java
@@ -0,0 +1,33 @@
+package shop.genieus.product.application.in.query;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.genieus.product.application.in.command.dto.FindProductCommand;
+import shop.genieus.product.application.out.cache.ProductCachePort;
+import shop.genieus.product.application.out.persistence.ProductCommandPort;
+import shop.genieus.product.domain.model.ProductView;
+import shop.genieus.product.domain.model.entity.Product;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProductQueryService {
+  private final ProductCachePort productCachePort;
+  private final ProductCommandPort productCommandPort;
+
+  public ProductView getProduct(FindProductCommand command) {
+    Long productId = command.productId();
+    ProductView view = productCachePort.findProductViewById(productId);
+
+    if (view != null) return view;
+
+    Product product = productCommandPort.findProductById(productId);
+    productCachePort.saveProduct(productId, product);
+    view = productCachePort.findProductViewById(productId);
+
+    return view;
+  }
+}
