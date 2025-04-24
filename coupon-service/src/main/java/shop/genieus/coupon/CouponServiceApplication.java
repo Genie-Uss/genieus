9b1c5c4c7bbfca668a68b304d02652eeb4353174
ryujy <bamdal124@gmail.com>
2025-04-24 20:58:39 +0900

    Fix: Coupon - 쿠폰 서비스 종료되는 이슈 해결을 위한 수정
    - yml파일 간소화
    - batch runner의 exit 주석
    - logback 삭제

--- coupon-service/src/main/java/shop/genieus/coupon/CouponServiceApplication.java
+++ coupon-service/src/main/java/shop/genieus/coupon/CouponServiceApplication.java
@@ -1,11 +1,9 @@
 package shop.genieus.coupon;
 
-import com.genieus.common.annotation.EnableCommonLib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableCommonLib
 public class CouponServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(CouponServiceApplication.class, args);
