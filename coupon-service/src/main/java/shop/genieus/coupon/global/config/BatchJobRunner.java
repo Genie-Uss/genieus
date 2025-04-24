2330798a2466c6af62ea6956ae74805574d850a8
ryujy <bamdal124@gmail.com>
2025-04-24 15:50:20 +0900

    Refactor: Coupon - 쿠폰 배포 프로필 수정
    - 배치 프로필 배포와 로컬로 구분

--- coupon-service/src/main/java/shop/genieus/coupon/global/config/BatchJobRunner.java
+++ coupon-service/src/main/java/shop/genieus/coupon/global/config/BatchJobRunner.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("batch")
+@Profile({"batch-dev", "batch-local"})
 @RequiredArgsConstructor
 public class BatchJobRunner implements CommandLineRunner {
 
