40cca3be42ca15ad04b3d736b088e922434f4d41
sosa7753 <sosa7753@naver.com>
2025-04-25 17:01:20 +0900

    Refactor: 서버 시작시 당일 최저가 갱신 로직 추가

---
+++ promotion-service/src/main/java/shop/genieus/promotion/application/system/runner/TodayLowestRateRunner.java
@@ -0,0 +1,25 @@
+package shop.genieus.promotion.application.system.runner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import shop.genieus.promotion.application.system.PromotionProductService;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TodayLowestRateRunner implements ApplicationRunner {
+
+    private final PromotionProductService promotionProductService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        LocalDateTime checkDay = LocalDateTime.now().minusDays(1);
+        promotionProductService.findMaxDiscountRateProductsByDate(checkDay);
+        log.info("당일 최저가 갱신 완료");
+    }
+}
