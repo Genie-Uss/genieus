cd31892a70ad48c66ba4d398470b3aa2c12fb816
seongmin <seongmin1117@gmail.com>
2025-04-25 22:07:19 +0900

    Fix: 주문취소 가능 상태 수정

--- order-service/src/main/java/shop/genieus/order/domain/model/vo/OrderStatus.java
+++ order-service/src/main/java/shop/genieus/order/domain/model/vo/OrderStatus.java
@@ -10,7 +10,7 @@ public enum OrderStatus {
   ORDER_PENDING("주문대기", true),
   PAYMENT_PENDING("결제대기", true),
   PAYMENT_COMPLETED("결제완료", true),
-  ORDER_COMPLETED("주문완료", false),
+  ORDER_COMPLETED("주문완료", true),
   ORDER_CANCELLED("주문취소", false),
   ORDER_EXPIRED("주문만료", false),
   ;
