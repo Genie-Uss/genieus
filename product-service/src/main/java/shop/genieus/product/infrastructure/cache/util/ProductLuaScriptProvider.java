c6ec385c4fbfe66b1b020c531eabadc03ec2bf9b
Soobin Kim <xpsxm225@naver.com>
2025-04-25 17:25:25 +0900

    Feat: 주문 취소 및 만료 이벤트 컨슘 로직 구현
    - dedup key를 통한 멱등성 처리를 구현했습니다.
    - Lua Script에서 빌드된 로그 메시지를 출력합니다.
    
    Resolved #82

--- product-service/src/main/java/shop/genieus/product/infrastructure/cache/util/ProductLuaScriptProvider.java
+++ product-service/src/main/java/shop/genieus/product/infrastructure/cache/util/ProductLuaScriptProvider.java
@@ -9,7 +9,8 @@ import org.springframework.scripting.support.ResourceScriptSource;
 public class ProductLuaScriptProvider {
 
   private static final RedisScript<List> VALIDATE_AND_DECREASE_SCRIPT;
-  private static final RedisScript<List> RESTORE_STOCK_SCRIPT;
+  private static final RedisScript<List> DECREASE_USED_STOCK_SCRIPT;
+  private static final RedisScript<List> DECREASE_TOTAL_STOCK_SCRIPT;
   private static final RedisScript<List> TOTAL_STOCK_DECREASE_SCRIPT;
 
   static {
@@ -19,15 +20,21 @@ public class ProductLuaScriptProvider {
     stockDecreaseScript.setResultType(List.class);
     VALIDATE_AND_DECREASE_SCRIPT = stockDecreaseScript;
 
-    DefaultRedisScript<List> stockRestoreScript = new DefaultRedisScript<>();
-    stockRestoreScript.setScriptSource(
-        new ResourceScriptSource(new ClassPathResource("redis/restore-stock-script.lua")));
-    stockRestoreScript.setResultType(List.class);
-    RESTORE_STOCK_SCRIPT = stockRestoreScript;
+    DefaultRedisScript<List> decreaseUsedStockScript = new DefaultRedisScript<>();
+    decreaseUsedStockScript.setScriptSource(
+        new ResourceScriptSource(new ClassPathResource("redis/restore-used-stock-script.lua")));
+    decreaseUsedStockScript.setResultType(List.class);
+    DECREASE_USED_STOCK_SCRIPT = decreaseUsedStockScript;
+
+    DefaultRedisScript<List> decreaseTotalStockScript = new DefaultRedisScript<>();
+    decreaseTotalStockScript.setScriptSource(
+        new ResourceScriptSource(new ClassPathResource("redis/decrease-total-stock-script.lua")));
+    decreaseTotalStockScript.setResultType(List.class);
+    DECREASE_TOTAL_STOCK_SCRIPT = decreaseTotalStockScript;
 
     DefaultRedisScript<List> totalStockDecreaseScript = new DefaultRedisScript<>();
     totalStockDecreaseScript.setScriptSource(
-            new ResourceScriptSource(new ClassPathResource("redis/order-completed.lua")));
+        new ResourceScriptSource(new ClassPathResource("redis/order-completed.lua")));
     totalStockDecreaseScript.setResultType(List.class);
     TOTAL_STOCK_DECREASE_SCRIPT = totalStockDecreaseScript;
   }
@@ -36,8 +43,12 @@ public class ProductLuaScriptProvider {
     return VALIDATE_AND_DECREASE_SCRIPT;
   }
 
-  public static RedisScript<List> getRestoreStockWithEventsScript() {
-    return RESTORE_STOCK_SCRIPT;
+  public static RedisScript<List> getDecreaseUsedStockScript() {
+    return DECREASE_USED_STOCK_SCRIPT;
+  }
+
+  public static RedisScript<List> getDecreaseTotalStockScript() {
+    return DECREASE_TOTAL_STOCK_SCRIPT;
   }
 
   public static RedisScript<List> getTotalStockDecreaseScript() {
