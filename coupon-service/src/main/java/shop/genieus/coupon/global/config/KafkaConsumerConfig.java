82b4a8bcb61e72bfc51aed95f038a50ae11e693c
ryujy <bamdal124@gmail.com>
2025-04-25 18:11:39 +0900

    Fix: Coupon - batch 서비스에서 kafka 인식하는 오류 해결

--- coupon-service/src/main/java/shop/genieus/coupon/global/config/KafkaConsumerConfig.java
+++ coupon-service/src/main/java/shop/genieus/coupon/global/config/KafkaConsumerConfig.java
@@ -7,17 +7,18 @@ import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZE
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 @EnableKafka
 @Configuration
+@Profile("dev")
 public class KafkaConsumerConfig {
 
   @Value("${spring.kafka.bootstrap-servers}")
