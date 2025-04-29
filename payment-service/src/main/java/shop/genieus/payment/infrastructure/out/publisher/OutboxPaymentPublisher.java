87bdb541f6274058f938ac848545d218a8cf8dfb
zun <k5orkrj@gmail.com>
2025-04-29 11:20:20 +0900

    Feat: 스케줄러를 이용하여 polling 방식 아웃박스 패턴 구현 (테스트 전) #100

---
+++ payment-service/src/main/java/shop/genieus/payment/infrastructure/out/publisher/OutboxPaymentPublisher.java
@@ -0,0 +1,70 @@
+package shop.genieus.payment.infrastructure.out.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.genieus.common.event.EventEnvelope;
+import com.genieus.common.event.payment.PaymentCompletedEvent;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import shop.genieus.payment.global.exception.PaymentJsonMappingException;
+import shop.genieus.payment.global.exception.PaymentJsonParsingException;
+import shop.genieus.payment.infrastructure.out.entity.Outbox;
+import shop.genieus.payment.infrastructure.out.persistence.PaymentOutboxAdapter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OutboxPaymentPublisher {
+
+  private final PaymentOutboxAdapter paymentOutboxAdapter;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  private final ObjectMapper objectMapper;
+
+  @Transactional
+  @Scheduled(fixedDelay = 5000)
+  public void publishCompletedPaymentEvents() {
+    List<Outbox> eventsNotPublished = paymentOutboxAdapter.findEventsNotPublished();
+
+    /**
+     * 직렬화된 데이터를 다시 역직렬화..?
+     */
+    for (Outbox outbox : eventsNotPublished) {
+      log.info("[아웃박스 발행] 시작: {}", outbox.getPaymentOutboxId() + " " + outbox.getIsPublished());
+
+      log.info("[결제 완료 이벤트 시작] 결제 완료 이벤트 발행 시작");
+      var event = getPaymentCompletedEvent(outbox);
+      var eventEnvelope = EventEnvelope.create(event);
+
+      try {
+        String json = objectMapper.writeValueAsString(eventEnvelope);
+        kafkaTemplate.send("payment-events", json);
+        log.info("[결제 완료 이벤트 성공] 결제 완료 이벤트 발행 완료: {}", json);
+
+        outbox.markPublished();
+        log.info("[아웃박스 발행] 완료: {}", outbox.getPaymentOutboxId() + " " + outbox.getIsPublished());
+
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private PaymentCompletedEvent getPaymentCompletedEvent(Outbox outbox) {
+    try {
+      return objectMapper.readValue(outbox.getEvent(), PaymentCompletedEvent.class);
+
+    } catch (InvalidFormatException ife) {
+      throw new PaymentJsonMappingException(ife);
+
+    } catch (JsonProcessingException jpe) {
+      throw new PaymentJsonParsingException(jpe);
+    }
+  }
+}
+
