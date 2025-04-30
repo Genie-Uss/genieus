16a1c8b290d3607a9cf58d9d9ef24b814b5a91f4
seongmin <seongmin1117@gmail.com>
2025-04-30 10:47:41 +0900

    Feat: 쿠폰 복구 이벤트 컨슘 구현

--- coupon-service/src/main/java/shop/genieus/coupon/presentation/event/CouponKafkaEventHandler.java
+++ coupon-service/src/main/java/shop/genieus/coupon/presentation/event/CouponKafkaEventHandler.java
@@ -4,6 +4,7 @@ import com.genieus.common.event.DeadLetterEnvelope;
 import com.genieus.common.event.EventEnvelope;
 import com.genieus.common.event.annotation.EventTypeMapping;
 import com.genieus.common.event.annotation.FallbackMapping;
+import com.genieus.common.event.order.CouponRestoredEvent;
 import com.genieus.common.event.order.OrderCanceledEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,10 +32,16 @@ public class CouponKafkaEventHandler {
     commandService.cancelCoupon(couponId, userId);
   }
 
+  @EventTypeMapping(topic = "${spring.kafka.consumer.topic.order}")
+  public void handleCouponRestored(CouponRestoredEvent event) {
+    log.info("[handleCouponRestored] 쿠폰 복구 이벤트 수신 : {}", event);
+    commandService.cancelCoupon(event.couponId(), event.userId());
+  }
+
   @FallbackMapping(topic = "order-events", eventType = "OrderCanceledEvent")
   public void handleOrderCanceledFallback(
       EventEnvelope<OrderCanceledEvent> envelope, Throwable ex) {
-    log.error("[주문 트리거 이벤트 실패 - fallback] envelope:{}, error:{} ", envelope, ex.getMessage());
+    log.error("[주문 취소 이벤트 처리 실패 - fallback] envelope:{}, error:{} ", envelope, ex.getMessage());
     DeadLetterEnvelope<OrderCanceledEvent> deadLetterEnvelope =
         DeadLetterEnvelope.from(envelope, ex.getMessage());
   }
